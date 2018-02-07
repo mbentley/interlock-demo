d97d8331d11da4527b4f47ed73025903ae3fbb1a
Matt Bentley <mbentley@mbentley.net>
2018-02-07 11:08:42 -0500

    Added sleep between scaling
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- 6_scale_apps.sh
+++ 6_scale_apps.sh
@@ -8,8 +8,15 @@ echo "=================================="
 # talk to docker1
 export DOCKER_HOST="tcp://localhost:1001"
 
-# scale both apps to 3 replicas
-docker service scale demo-east=3 demo-west=3
+# scale demo-east to 3 replicas
+docker service scale demo-east=3
+
+# sleep
+echo "Sleeping 15 seconds between scaling..."
+sleep 15
+
+# scale demo-west to 3 replicas
+docker service scale demo-west=3
 
 echo "=================================="
 echo "End scaling multi-region apps!"
